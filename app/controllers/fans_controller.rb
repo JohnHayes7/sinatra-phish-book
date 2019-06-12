3a6c62db41a7d3d2deaefd9918c9ae7a07dc85d8
John Hayes <JohnnyHayes7@gmail.com>
2019-06-12 08:05:49 -0400

    built in authentication and login/logout functionality

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -11,7 +11,9 @@ class FansController < ApplicationController
     end
 
     get '/fans/:slug' do
+        if logged_in?(session)
        @fan = Fan.find_by_slug(params[:slug])
+        end
         
     erb :'/fans/homepage'
     end
@@ -20,11 +22,28 @@ class FansController < ApplicationController
         @fan = Fan.find_by_username(params[:username])
         if @fan == nil
             @fan = Fan.create(:username => params[:username], :email => params[:email], :password => params[:password])
+            session[:user_id] = @fan.id
+            redirect :"/fans/#{@fan.slug}"
+        else
+            redirect :'fans/signup'
         end
-        
-        
-        
-    redirect :"/fans/#{@fan.slug}"
+    end
+
+    post '/fans/login' do
+       @fan = Fan.find_by_username(params[:username])
+       
+       if @fan && @fan.authenticate(params[:password])
+        session[:id] = @fan.id
+        redirect "/fans/#{@fan.slug}"
+       else
+        redirect '/fans/login'
+       end
+    end
+
+    post '/fans/logout' do 
+        session.clear
+
+    erb :welcome
     end
 
 end
