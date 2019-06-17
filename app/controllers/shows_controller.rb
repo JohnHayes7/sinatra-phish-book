35533ab425ef2d4aeb59aa733d07bf55c25407de
John Hayes <JohnnyHayes7@gmail.com>
2019-06-17 10:42:44 -0400

    added shows to years

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -4,14 +4,24 @@ class ShowsController < ApplicationController
     @year = Year.find_by(:value => params[:year])
         
         if @year.shows.empty?
-             shows = Show.all.detect do |s| 
-                s[:date].split(" ")[1].split("/").last.to_i == @year.value
-                binding.pry
+             shows = Show.all.select {|s| s[:date].split(" ")[1].split("/").last.to_i == @year.value}
+             
+                if shows == nil
+                    Scraper.show_info_scraper(@year.value)
+                    added_shows = Show.all.select {|s| s[:date].split(" ")[1].split("/").last.to_i == @year.value}
+                    added_shows.each do |s|
+                        s.year_id = @year.id
+                        s.save
+                    end
+                else
+                    shows.each do |s|
+                        s.year_id = @year.id
+                        s.save
+                    end
                 end
-            
-            # Scraper.show_info_scraper(@year.value)
-            
         end
+
+        binding.pry
     erb :'shows/show'
     end
 
