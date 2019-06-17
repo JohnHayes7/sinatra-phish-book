fa3769f117ac368d198a6506ba7c413fa08e63a3
John Hayes <JohnnyHayes7@gmail.com>
2019-06-17 13:50:06 -0400

    reorganized years/shows responsibilities

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,30 +1,9 @@
 class ShowsController < ApplicationController
     
-    get '/shows/:year' do
-    @year = Year.find_by(:value => params[:year])
-        #SHOULD REFACTOR BELOW
-        if @year.shows.empty?
-             shows = Show.all.select {|s| s[:date].split(" ")[1].split("/").last.to_i == @year.value}
-             
-                if shows.empty?
-                    Scraper.show_info_scraper(@year.value)
-                    added_shows = Show.all.select {|s| s[:date].split(" ")[1].split("/").last.to_i == @year.value}
-                    added_shows.each do |s|
-                        s.year_id = @year.id
-                        s.save
-                    end
-                else
-                    shows.each do |s|
-                        s.year_id = @year.id
-                        s.save
-                    end
-                end
-        end
-       
-    erb :'years/show'
-    end
+    # get '/shows/:year' do
+    
 
-    get '/show/:date_slug' do
+    get '/shows/:date_slug' do
 
     end
 
--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -7,6 +7,29 @@ class YearsController < ApplicationController
     erb :'years/index'
     end
     
+    get '/years/:year' do
+    @year = Year.find_by(:value => params[:year])
+    #SHOULD REFACTOR BELOW
+        if @year.shows.empty?
+            shows = Show.all.select {|s| s[:date].split(" ")[1].split("/").last.to_i == @year.value}
+            
+                if shows.empty?
+                    Scraper.show_info_scraper(@year.value)
+                    added_shows = Show.all.select {|s| s[:date].split(" ")[1].split("/").last.to_i == @year.value}
+                    added_shows.each do |s|
+                        s.year_id = @year.id
+                        s.save
+                    end
+                else
+                    shows.each do |s|
+                        s.year_id = @year.id
+                        s.save
+                    end
+                end
+        end
+   
+    erb :'years/show'
+    end
 
     
 end
