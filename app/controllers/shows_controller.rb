58f5733efa405f355a41baf08ee289a564224365
John Hayes <JohnnyHayes7@gmail.com>
2019-06-17 15:24:45 -0400

    improved navigation links

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,7 +3,8 @@ class ShowsController < ApplicationController
 
     get '/shows/:date_slug' do
         @show = Show.find_by_slug(params[:date_slug])
-        
+        @current_user = current_user(session)
+        # binding.pry
         erb :'/shows/show'
     end
 
